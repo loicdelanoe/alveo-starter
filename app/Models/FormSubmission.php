16d19675f96cb520e59631290a1685be19b05c2d
Loïc Delanoë <95628210+loicdelanoe@users.noreply.github.com>
2025-06-25 12:34:41 +0200

    imp: 🎨 format date properly

--- app/Models/FormSubmission.php
+++ app/Models/FormSubmission.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
 
 class FormSubmission extends Model
 {
@@ -24,5 +26,19 @@ class FormSubmission extends Model
     protected function form(): BelongsTo
     {
         return $this->belongsTo(Form::class);
+    }
+
+    protected function createdAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => Carbon::parse($value)->format('Y-m-d H:i:s'),
+        );
+    }
+
+    protected function updatedAt(): Attribute
+    {
+        return Attribute::make(
+            get: fn (string $value) => Carbon::parse($value)->format('Y-m-d H:i:s'),
+        );
     }
 }
